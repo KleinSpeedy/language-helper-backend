d1af46a75f44a9d41cff991e6abf1f428b19bda6
jschulze <joni.schulze@t-online.de>
2023-07-09 17:34:49 +0200

    Preapare separation of integration and deployment
    quick fix for first linting error

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -70,5 +71,5 @@ func setupRouter() *gin.Engine {
 func main() {
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	log.Fatalln(r.Run(":8080"))
 }
